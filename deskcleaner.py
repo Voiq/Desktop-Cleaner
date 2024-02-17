c5e2fffab80ba14d3a2020ccb1ef476765675900
Ahmed Yasser <113800339+Voiq@users.noreply.github.com>
2024-02-17 13:16:52 +0200

    Update deskcleaner.py

--- deskcleaner.py
+++ deskcleaner.py
@@ -6,8 +6,8 @@ from EventHandler import EventHandler
 
 
 if __name__ == "__main__":
-    folder_to_track = "C:/Users/Ahmed Yasser/Downloads"
-    folder_destination = "E:/Memez"
+    folder_to_track = "Folder_to_track"
+    folder_destination = "target_folder"
     watch_path = Path(folder_to_track)
     destination_root = Path(folder_destination)
     event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
